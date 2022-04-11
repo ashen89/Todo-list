70f3daa88a9bd9da33b59f94f75232d283297407
Ashen <ashensachintha@xyicon.com>
2022-04-11 17:20:43 +0530

    useState created for todolist

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField";
 
 const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+
   return (
     <div className="App">
       <span className="heading">Hello World</span>
-      <InputField />
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 };
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./styles.css";
 
-const InputField = () => {
+interface Props{
+  todo:string;
+  setTodo:React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField = ({todo, setTodo}: Props) => {
+  console.log(todo);
   return (
     <form className="input">
-      <input type="input" placeholder="Enter a task" className="input_box" />
+      <input value={todo} onChange={(e)=> setTodo(e.target.value)} 
+      type="input" placeholder="Enter a task" className="input_box" />
       <button className="input_submit">Go</button>
     </form>
   );
