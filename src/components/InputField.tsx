4986bce0534059cf254f46f821d131b7321383cb
Ashen <ashensachintha@xyicon.com>
2022-04-12 10:53:03 +0530

    todos useState created, useRef to shift focus from field.

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { FormEvent, useRef } from "react";
 import "./styles.css";
 
 interface Props{
   todo:string;
   setTodo:React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField = ({todo, setTodo}: Props) => {
-  console.log(todo);
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  
   return (
-    <form className="input">
-      <input value={todo} onChange={(e)=> setTodo(e.target.value)} 
+    <form className="input" onSubmit={(e)=> {
+      handleAdd(e);
+      inputRef.current?.blur(); //When you click the go button automatically reverse to light bakcground
+      }}>
+      <input ref={inputRef} value={todo} onChange={(e)=> setTodo(e.target.value)} 
       type="input" placeholder="Enter a task" className="input_box" />
       <button className="input_submit">Go</button>
     </form>
